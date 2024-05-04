d038927439ab326de0adaa95edc1fdcac9e25d21
divu72k <77772116+divu72k@users.noreply.github.com>
2024-05-04 23:58:25 +0530

    Add files via upload

--- Python_Assignment/03_Python_Assignment/q2.py
+++ Python_Assignment/03_Python_Assignment/q2.py
@@ -1,20 +1,18 @@
-def extract_pattern_from_file(file_path, pattern):
-    matches=[]
-    total_count=0
+def search_pattern(file_name, pattern):
+    try:
+        with open(file_name, 'r') as f:
+            lines = f.readlines()
+            matching_lines = [line.strip() for line in lines if pattern in line]
+            return matching_lines
+    except Exception as e:
+        print(str(e))
+        return []
     
-    with open(file_path,'r') as file:
-        for line in file:
-            line_count=line.count(pattern)
-            total_count+=line_count
-            if line_count>0:
-                matches.extend([line[i:i+line_count] for i in range(len(line)) if line.startswith(pattern)])
-    
-    return matches,total_count
 
-file_path=input("enter the path of the file:")
+
+file_path=input("enter the name of the file:")
 pattern=input("enter the string pattern to be searched:")
-matches,total_count=extract_pattern_from_file(file_path, pattern)
-print("Total Occurrences:",total_count)
+matches=search_pattern(file_path,pattern)
 print("Matches:")
 for match in matches:
     print(match)
